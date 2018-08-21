3a9d8d5f5e8d39dcce4bad518ff2432df36ab877
amchercashin <amchercashin@gmail.com>
2018-08-21 16:02:14 +0300

    revert back rebalance if less don't like it

--- misc.js
+++ misc.js
@@ -3,10 +3,6 @@ function updateSliderLables() {
     document.getElementById("RGBITR-share").innerText = this.value;
 }
 
-function updateRebalanceSliderLables() {
-    document.getElementById("rebalance-value").innerText = this.value;
-}
-
 async function extract(data, dateCol = 2, valueCol = 5) {
     return { x: data.map(row => row[dateCol]), 
              y: data.map(row => row[valueCol]) 
